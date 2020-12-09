7c3390058ed18c8900e1a30966e0f7b48307e3c9
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-12-09 20:51:46 +0000

    For a list of 2 elements, the scanner returns their product

--- d01e01/test/scanner_test.dart
+++ d01e01/test/scanner_test.dart
@@ -1,8 +1,10 @@
 import 'package:test/test.dart';
 
 class Scanner {
-  static int scan(numbers) {
-    return 0;
+  static int scan(List<int> numbers) {
+    final x = numbers.first;
+    final y = numbers.elementAt(1);
+    return x * y;
   }
 }
 
@@ -10,5 +12,10 @@ void main() {
   test('Given a list containing 2020 and 0, returns 0', () {
     const numbers = [2020, 0];
     expect(Scanner.scan(numbers), equals(0));
+  });
+
+  test('Given a list containing 2019 and 1, returns 2019', () {
+    const numbers = [2019, 1];
+    expect(Scanner.scan(numbers), equals(2019));
   });
 }
